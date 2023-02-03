d51dcb270fd06555495e781f69efb62a5f33de5f
SubodhACloudEngineer <83665151+SubodhACloudEngineer@users.noreply.github.com>
2023-02-03 13:11:24 +0530

    Update SNMP.php

--- Topology/Topology_Types/SNMP.php
+++ Topology/Topology_Types/SNMP.php
@@ -9,15 +9,30 @@ function calculateDeviceTopology($deviceId, $name, $device_nature, $status) {
 	logTofile("*** calculateDeviceTopology  deviceId: $deviceId name: $name status: $status \n");
 
 	$nodePlace = createTopology($deviceId, $name, $device_nature, "router", "", $status);
-	$error = readInformationsFromDevice($deviceId, $community, $address);
-	
-	if ($error == "") {
+	$response = _configuration_variable_list($context['device_id']);
+	$response = json_decode($response, true);
+	$device_id = $context['device_id'];
+	$conf_snmp = $response['wo_newparams'];
+	$check_snmp = array("snmpv3_authKey","snmpv3_authProtocol","snmpv3_privKey","snmpv3_privProtocol","snmpv3_securityEngineID","snmpv3_securityLevel","snmpv3_securityName");
+	$reg = array();
+	$keys = array_keys($conf_snmp);
+	foreach($check_snmp as $d) {
+		for($i = 0; $i < count($conf_snmp); $i++) {
+			foreach($conf_snmp[$keys[$i]] as $key => $value) {
+				if (($key == "name") && ($d == $value)) {
+					$reg[] = $value;
+				}
+			}
+		}
+	}
+	$result=array_diff($check_snmp,$reg);
+	if(count($result) == 0) {
 		try {
 			checkSNMPResponds($community, $address);
-			$cmd = "snmpwalk -v2c -c $community $address IP-MIB::ipAdEntNetMask 2>&1";
+			$cmd = "snmpwalk -v3 -l $snmpv3_securityLevel -u $username -a $snmpv3_authProtocol -A $snmpv3_authKey $address";
 			logTofile("*** calculateDeviceTopology SNMP COMMAND: ".$cmd."\n");
 			exec($cmd, $value, $error);
-			if (!$error) {
+			if (!count($result)) {
 				foreach ($value as $search) {
 					if (searchAdress($search, $matches) != false) {
 						logTofile(debug_dump($matches, "checkSNMPResponds searchAdress matches:\n"));
@@ -41,8 +56,40 @@ function calculateDeviceTopology($deviceId, $name, $device_nature, $status) {
 	} else {
 		logTofile(debug_dump($error, "*** calculateDeviceTopology ERROR_3 ***\n"));
 	}
+	else {
+		$error = readInformationsFromDevice($deviceId, $community, $address);
+		if ($error == "") {
+			try {
+				checkSNMPResponds($community, $address);
+				$cmd = "snmpwalk -v2c -c $community $address IP-MIB::ipAdEntNetMask 2>&1";
+				logTofile("*** calculateDeviceTopology SNMP COMMAND: ".$cmd."\n");
+				exec($cmd, $value, $error);
+				if (!$error) {
+					foreach ($value as $search) {
+						if (searchAdress($search, $matches) != false) {
+							logTofile(debug_dump($matches, "checkSNMPResponds searchAdress matches:\n"));
+							if ($matches [1] [0] != 127) {
+								$address_link = $matches [0] [0];
+								$maskAdr = $matches [0] [1];
+								$mask = calcMask($maskAdr);
+								$address_link_masked = getNetworkByAddressAndMask($address_link, $mask);
+								$addressAndMask = $address_link_masked . "/" . $mask;
+								createTopologyNetwork(str_replace(".", "_", $addressAndMask), $addressAndMask, "network", "");
+								$context ['Nodes'] [$nodePlace] ["links"] [] = $addressAndMask;
+							}
+						}
+					}
+				} else {
+					logTofile(debug_dump($value, "*** calculateDeviceTopology ERROR_1***\n"));
+				}
+			} catch (Exception $e) {
+				logTofile(debug_dump($e->getMessage(), "*** calculateDeviceTopology ERROR_2***\n"));
+			}
+		} else {
+			logTofile(debug_dump($error, "*** calculateDeviceTopology ERROR_3 ***\n"));
+		}
+	}
 }
-
 function checkSNMPResponds($community, $address) {
 	$cmd_SNMP_RESPOND = "timeout 1 snmpwalk -v2c -c $community $address SNMPv2-MIB::sysName 2>&1";
 	logToFile("checkSNMPResponds with command: " . $cmd_SNMP_RESPOND  . "\n");
