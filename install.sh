80344a7529d0c5f7c1a632d31ef499e170dfd442
Olivier Vignaud <ovi@ubiqube.com>
2023-10-27 12:25:36 +0200

    MSA-13292: Add install script.

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+. /usr/share/install-libraries/il-lib.sh
+
+pushd /opt/fmc_repository/Process || exit;
+emit_step "Linking %b" "Topology"
+mk_meta_link "OpenMSA_Workflow_Topology" "Topology"
+popd || exit
